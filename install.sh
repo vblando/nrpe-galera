782bb3490fe601967b0b871b957a78bd72d1558b
Vladimir Blando <vladimir.blando@gmail.com>
2019-02-10 23:56:38 +0800

    fixed echo error

--- install.sh
+++ install.sh
@@ -2,9 +2,7 @@
 
 # copy the check to the nagios plugins dir
 echo copying the 3 checks to the nagios plugins dir
-echo.
 cp -v check_galera_* /usr/lib64/nagios/plugins
 
 echo copying the nrpe config file to the nrpe configs dir
-echo.
 cp -v mysql_galera.cfg /etc/nrpe.d
